7f72786ffa2d9826ae52824842e3ab3ec8fa4276
zfac103 <zfac103@live.rhul.ac.uk>
2020-07-09 13:11:38 +0100

    Test 1: start game

--- Test/test_dice_sim.py
+++ Test/test_dice_sim.py
@@ -1,3 +1,11 @@
 import unittest
 
+
+class Dice(object):
+    pass
+
+
 class TestDiceSim(unittest.TestCase):
+    def test_start_game(self):
+        dice = Dice([])
+        self.assertEqual(dice.start_total(), "Press start")
